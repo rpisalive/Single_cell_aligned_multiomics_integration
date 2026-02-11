aee67d577274257174493f434aa83314adf7fd57
Man Ho PANG <mmenpang@gmail.com>
2026-02-11 22:21:27 +0000

    Updated scripts.

--- Script/1_Single_cell_pipeline/4_MOFA_downstream.R
+++ Script/1_Single_cell_pipeline/4_MOFA_downstream.R
@@ -1,35 +1,49 @@
 library(ggplot2)
 library(dplyr)
 library(MOFA2)
+library(svglite)
+library(ggokabeito)
 
 setwd("C:/Users/49152/Downloads/Multi-omics/MOFA/")
 model <- load_model("output/single_cell_trained_model.hdf5")
+outdir <- "C:/Users/49152/Downloads/Multi-omics/MOFA/output/graphs/Single_cell/Downstream_general/"
+
+theme_set(theme_bw(base_size = 16, base_family = "Arial"))
 
 plot_data_overview(model)
 model@samples_metadata
-model@samples_metadata <- model@samples_metadata %>%
+model@samples_metadata <- model@samples_metadata %>% 
   mutate(cell_line = sub("^[^_]+_([^_]+).*", "\\1", sample))
 model@samples_metadata
 
 # Total variance explained per view and group
 model@cache$variance_explained$r2_total
 model@cache$variance_explained$r2_per_factor
-plot_variance_explained(model, x="view", y="factor", plot_total = FALSE)
+p_var <- plot_variance_explained(model, x="view", y="factor", plot_total = FALSE)
+ggsave(filename = file.path(outdir, "Figure2A.svg"),plot = p_var,
+       device = svglite, width = 8,height = 6)
 plot_variance_explained(model, x="view", y="factor", plot_total = TRUE)
 
-plot_factor(model, factor = 1:6, color_by = "cell_line")
+p_factor <- plot_factor(model, factor = 1:6, color_by = "cell_line")
+p_factor <- plot_factor(model, factor = 1:6, color_by = "cell_line", dot_size = 1) +
+  theme_bw(base_size = 12) +
+  scale_colour_okabe_ito() +
+  theme(axis.title = element_text(size = 12),
+        axis.text  = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.text  = element_text(size = 12))
 
-p <- plot_factor(model, 
-                 factors = c(1,2,3,4,5,6),
+ggsave(filename = file.path(outdir, "Figure2B.svg"),
+       plot = p_factor,
+       device = svglite,
+       width = 6,height = 4)
+
+p <- plot_factor(model, factors = c(1,2,3,4,5,6),
                  color_by = "cell_line",
-                 dot_size = 3,        # change dot size
-                 dodge = T,           # dodge points with different colors
-                 legend = F,          # remove legend
-                 add_violin = T,      # add violin plots,
-                 violin_alpha = 0.25  # transparency of violin plots
-)
+                 dot_size = 3, dodge = T,
+                 legend = F, add_violin = T,
+                 violin_alpha = 0.25)
 
-# The output of plot_factor is a ggplot2 object that we can edit
 p <- p + 
   scale_color_manual(values=c("C10"="black", "SVEC"="red")) +
   scale_fill_manual(values=c("C10"="black", "SVEC"="red"))
@@ -38,112 +52,66 @@ print(p)
 
 plot_factors(model, 
              factors = 1:6,
-             color_by = "cell_line"
-)
+             color_by = "cell_line")
 
 
 views <- c("Transcriptomics", "Proteomics")
 K <- get_dimensions(model)$K
 factors <- seq_len(K)
 
-outdir <- "C:/Users/49152/Downloads/Multi-omics/MOFA/output/graphs/Single_cell/Downstream_general/"
-
 for (v in views) {
   for (f in factors) {
     
     message("Processing: ", v, " | Factor ", f)
     
-    # =====================================================
-    # 1. Weights (ggplot)
-    # =====================================================
-    p_weights <- plot_weights(
-      model,
-      view = v,
-      factor = f,
-      nfeatures = 10,
-      scale = TRUE,
-      abs = FALSE,
-      text_size = 2
-    )
+    #Weights plot
+    p_weights <- plot_weights(model,view = v,factor = f,nfeatures = 10,scale = TRUE,
+                              abs = FALSE,text_size = 2)
     
-    ggsave(
-      file.path(outdir, paste0(v, "_F", f, "_weights.png")),
-      p_weights, width = 6, height = 4, dpi = 300
-    )
+    ggsave(file.path(outdir, paste0(v, "_F", f, "_weights.svg")),
+           p_weights, width = 6, height = 4, device = svglite)
     
+    #Top weights
+    p_top <- plot_top_weights(model,view = v,factor = f,nfeatures = 20)
     
-    # =====================================================
-    # 2. Top weights (ggplot)
-    # =====================================================
-    p_top <- plot_top_weights(
-      model,
-      view = v,
-      factor = f,
-      nfeatures = 20
-    )
+    ggsave(file.path(outdir, paste0(v, "_F", f, "_topweights.svg")),
+      p_top, width = 6, height = 4, device = svglite)
     
-    ggsave(
-      file.path(outdir, paste0(v, "_F", f, "_topweights.png")),
-      p_top, width = 6, height = 4, dpi = 300
-    )
+    #Scatter
+    p_scatter <- plot_data_scatter(model,view = v,factor = f,features = 12,
+                                   add_lm = TRUE,color_by = "cell_line")
     
+    ggsave(file.path(outdir, paste0(v, "_F", f, "_scatter.svg")),
+      p_scatter, width = 10, height = 8, device = svglite)
     
-    # =====================================================
-    # 3. Scatter (ggplot)
-    # =====================================================
-    p_scatter <- plot_data_scatter(
-      model,
-      view = v,
-      factor = f,
-      features = 12,
-      add_lm = TRUE,
-      color_by = "cell_line"
-    )
+    #Heatmap
+    png(file.path(outdir, paste0(v, "_F", f, "_heatmap.png")),width = 1800,
+        height = 1200,res = 300)
     
-    ggsave(
-      file.path(outdir, paste0(v, "_F", f, "_scatter.png")),
-      p_scatter, width = 10, height = 8, dpi = 300
-    )
-    
-    
-    # =====================================================
-    # 4. Heatmap (pheatmap → device)
-    # =====================================================
-    png(
-      file.path(outdir, paste0(v, "_F", f, "_heatmap.png")),
-      width = 1800,
-      height = 1200,
-      res = 300
-    )
-    
-    plot_data_heatmap(
-      model,
-      view = v,
-      factor = f,
-      features = 20,
-      cluster_rows = TRUE,
-      cluster_cols = TRUE,
-      show_rownames = TRUE,
-      show_colnames = TRUE,
-      fontsize_col = 4# hide crowded labels
-    )
-    
+    plot_data_heatmap(model,view = v,factor = f,features = 20,cluster_rows = TRUE,
+                      cluster_cols = TRUE,show_rownames = TRUE, show_colnames = TRUE,
+                      fontsize_col = 4)
     dev.off()
   }
 }
 
-# Run UMAP
+#Run UMAP
 set.seed(42)
-model <- run_umap(model)
+model <- run_umap(model)#, factors = c(1,2)) - Adjust the factors to include
 
-# Create the UMAP plot
-p_umap <- plot_dimred(model,  method = "UMAP",  color_by = "cell_line")
+p_umap <- plot_dimred(model,method = "UMAP",color_by = "cell_line",
+                      color_name = "Cell Line",shape_by = NULL,label = FALSE,
+                      dot_size = 2,legend = TRUE)
 
-# Save the plot to file
-ggsave( filename = file.path(outdir, "UMAP_cellline.png"),
+p_umap <- p_umap +
+  scale_colour_okabe_ito() +
+  theme(axis.title = element_text(size = 16),axis.text = element_text(size = 16),
+        legend.title = element_text(size = 16),legend.text = element_text(size = 16))
+
+ggsave(
+  filename = file.path(outdir, "UMAP_cellline.svg"),
   plot = p_umap,
-  width = 6,
-  height = 5,
-  dpi = 300
+  device = svglite,
+  width = 8,
+  height = 6
 )
-
